8d49949c8dbad9361d7711fc5dcb29660522115a
Justin Vaughn <jtv6445@g.rit.edu>
2020-10-11 22:47:49 -0400

    got a chunk working but it's pretty broken still

---
+++ server/models/Dog.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+let DogModel = {};
+
+const DogSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+
+  breed: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: false,
+  },
+
+  age: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+
+});
+
+DogSchema.statics.findByName = (name, callback) => {
+  const search = {
+    name,
+  };
+
+  return DogModel.findOne(search, callback);
+};
+
+// Create the cat model based on the schema. You provide it with a custom discriminator
+// (the name of the object type. Can be anything)
+// and the schema to make a model from.
+// Look at the model variable definition above for more details.
+DogModel = mongoose.model('Dog', DogSchema);
+
+// export our public properties
+module.exports = {
+  DogModel,
+  DogSchema,
+};
